00b965947db0a894021bc1d837ee9d827ebfe484
Valencia Richards <63933204+v-richards@users.noreply.github.com>
2021-03-20 21:24:36 -0400

    Fix bug that was causing the articles from the 1st page to be saved in all directories and refactor code

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,35 +1,47 @@
+# scraper.py
+
+# Import standard library modules.
 import string
-import requests
-import json
 import os
 import re
+
+# Import external modules.
+import requests
+import json
 from bs4 import BeautifulSoup
 
+# Set headers for requests.get()
+headers = {"Accept-Language": "en-US,en;q=0.5"}
 
+
+# Stage 1
 def get_quote(url):
     error_message = "Invalid quote resource!"
-    response = requests.get(url, headers={"Accept-Language": "en-US,en;q=0.5"})
-    if response.status_code != 200:
-        return error_message
     try:
-        content = json.loads(response.text)["content"]
+        r = requests.get(url, headers=headers)
+        content = json.loads(r.text)["content"]
+    except ConnectionError:
+        return error_message
     except KeyError:
+        return error_message
+    if r.status_code != 200:
         return error_message
     return content
 
 
-def get_title_and_description(url):
+# Stage 2
+def get_movie_title_and_description(url):
     content = {}
     error_message = "Invalid movie page!"
     if not url.startswith("https://www.imdb.com/title/"):
         return error_message
     try:
-        response = requests.get(url, headers={"Accept-Language": "en-US,en;q=0.5"})
+        r = requests.get(url, headers=headers)
     except ConnectionError:
         return error_message
-    if response.status_code != 200:
+    if r.status_code != 200:
         return error_message
-    soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(r.content, "html.parser")
     content["title"] = soup.find("h1").contents[0].strip()
     content["description"] = soup.find("div", class_="summary_text").contents[0].strip()
     try:
@@ -39,55 +51,66 @@ def get_title_and_description(url):
     return content
 
 
+# Stage 3
 def save_page_source_code(url):
-    response = requests.get(url, headers={"Accept-Language": "en-US,en;q=0.5"})
-    if response.status_code != 200:
-        return "The URL returned " + str(response.status_code) + "!"
+    r = requests.get(url, headers=headers)
+    if r.status_code != 200:
+        return "The URL returned " + str(r.status_code) + "!"
     else:
-        page_content = response.content
+        page_content = r.content
         with open("source.html", "wb") as file:
             file.write(page_content)
         return "Content saved."
 
 
-def save_articles(n_pages, article_type):
+# Stages 4 and 5
+def get_soup(url, page=None):
+    params = {"page": page}
+    try:
+        r = requests.get(url, headers=headers, params=params)
+    except ConnectionError as conn_error:
+        print(conn_error)
+        return None
+    return BeautifulSoup(r.content, "html.parser")
+
+
+def save_articles(n_pages, desired_article_type):
+    # Assign the default URL.
     url = "https://www.nature.com/nature/articles"
-    saved_articles = []
     for page in range(1, n_pages + 1):
+        # Create a directory to store articles.
         try:
             os.mkdir(f"/home/valencia/PycharmProjects/Web Scraper/Web Scraper/task/Page_{page}")
         except FileExistsError:
             pass
-        try:
-            response = requests.get(url, headers={"Accept-Language": "en-US,en;q=0.5"})
-        except ConnectionError as conn_error:
-            print(conn_error)
-            return None
-        data = response.content
-        soup = BeautifulSoup(data, "html.parser")
-        for article in soup.find_all("article"):
-            name = str(article.find("a").contents[0])
-            # Strip punctuation and replace spaces with underscores
-            name = name.translate(name.maketrans(" ", "_", string.punctuation)).replace("'", "")
-            href = article.find("a", {"data-track-action": "view article"})["href"].strip()
-            if article.find("span", {"data-test": "article.type"}).text.strip().lower() == article_type:
-                article_response = requests.get("https://www.nature.com" + href,
-                                                headers={"Accept-Language": "en-US,en;q=0.5"})
-                article_data = article_response.content
-                article_soup = BeautifulSoup(article_data, "html.parser")
-                regex = re.compile(".*body.*")
-                body = article_soup.find("div", {"class": regex}).text.strip()
-                with open(f"Page_{page}/{name}.txt", "wb") as file:
-                    file.write(body.encode("UTF-8"))
-                    saved_articles.append(name)
-        next_page_link = soup.find("a", class_="c-pagination__link")["href"].strip()
-        url = "https://www.nature.com" + next_page_link
-    return "Saved articles:\n", saved_articles
+        # Get soup from page.
+        soup = get_soup(url, page)
+        if soup:
+            # Iterate through each article on the page and, if it's of the desired type, save a copy of its
+            # content(body) in the folder for that page.
+            for article in soup.find_all("article"):
+                # Get article name, then strip punctuation and replace spaces with underscores
+                name = str(article.find("a").contents[0])
+                name = name.translate(name.maketrans(" ", "_", string.punctuation)).replace("'", "")
+                href = article.find("a", {"data-track-action": "view article"})["href"].strip()
+                article_type = article.find("span", {"data-test": "article.type"}).text.strip().lower()
+                if article_type == desired_article_type:
+                    # Get soup from article
+                    article_soup = get_soup("https://www.nature.com" + href)
+                    if article_soup:
+                        # Extract the body of the article and save it as bytes in a .txt file
+                        regex = re.compile(".*body.*")
+                        body = article_soup.find("div", {"class": regex}).text.strip()
+                        with open(f"Page_{page}/{name}.txt", "wb") as file:
+                            file.write(body.encode("UTF-8"))
+        else:
+            return "There was a problem reaching the website."
+    return "All articles saved."
 
 
 try:
     number_of_pages = int(input())
     type_of_article = input().lower().strip()
-    save_articles(number_of_pages, type_of_article)
+    print(save_articles(number_of_pages, type_of_article))
 except ValueError as error:
     print(error)
